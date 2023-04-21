72c0bda1a64c97a8634dd305bddfd82bbb497a23
jeison.alarcon <alarcon_osorio@hotmail.com>
2023-04-21 18:59:09 -0500

    Local Terpel - Test Unitarios OK y cambios en modulo cliente - ADD BD

---
+++ ejemplo/src/Main.java
@@ -0,0 +1,8 @@
+import java.math.BigInteger;
+
+public class Main {
+    public static void main(String[] args) {
+        String numdoc = "9999999999";
+        System.out.println("Hello world!");
+    }
+}
